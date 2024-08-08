81a324257b16c53d29d9e0cd5426838c01b50040
divyam751 <mi.divyam@gmail.com>
2024-08-08 14:00:47 +0530

    update

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -9,20 +9,20 @@ export const Github = () => {
   }, []);
 
   return (
-    <section className='github-section'>
-      <h1 className='heading'>
-        <span className='sauce'> My Contributions and Stats</span>
+    <section className="github-section">
+      <h1 className="heading">
+        <span className="sauce"> My Contributions and Stats</span>
       </h1>
-      <div data-aos='fade-up'>
+      <div data-aos="fade-up">
         <GitHubCalendar
           fontSize={16}
           blockSize={15}
           year={2023}
-          username='divyam751'
+          username="divyam751"
         />
       </div>
       <div
-        data-aos='fade-up'
+        data-aos="fade-up"
         style={{
           display: "flex",
           justifyContent: "center",
@@ -31,33 +31,33 @@ export const Github = () => {
         }}
       >
         <img
-          id='github-top-langs'
-          align='center'
-          src='https://github-readme-stats.vercel.app/api/top-langs/?username=divyam751&theme=neon&border_radius=2.7'
-          alt='divyam751'
+          id="github-top-langs"
+          align="center"
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=divyam751&theme=neon&border_radius=2.7"
+          alt="divyam751"
         />
       </div>
       <div
-        data-aos='fade-up'
+        data-aos="fade-up"
         style={{
           display: "flex",
           justifyContent: "center",
           gap: "10px",
           marginTop: "3rem",
         }}
-        id='github-stat'
+        id="github-stat"
       >
         <img
-          id='github-stats-card'
-          align='center'
-          src='https://github-readme-stats.vercel.app/api?username=divyam751&theme=neon&border_radius=2.7&show_icons=true'
-          alt='divyam751'
+          id="github-stats-card"
+          align="center"
+          src="https://github-readme-stats.vercel.app/api?username=divyam751&theme=neon&border_radius=2.7&show_icons=true"
+          alt="divyam751"
         />
         <img
-          id='github-streak-stats'
-          align='center'
-          src='https://github-readme-streak-stats.herokuapp.com/?user=divyam751&theme=neon&border_radius=2.7&date_format=M%20j%5B%2C%20Y%5D'
-          alt='divyam751'
+          id="github-streak-stats"
+          align="center"
+          src="https://github-readme-streak-stats.herokuapp.com/?user=divyam751&theme=neon&border_radius=2.7&date_format=M%20j%5B%2C%20Y%5D"
+          alt="divyam751"
         />
       </div>
     </section>
